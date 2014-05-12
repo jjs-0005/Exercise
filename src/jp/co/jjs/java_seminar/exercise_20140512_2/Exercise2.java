cd10d34794fc9218fec3c34df8e7bed6902716d4
jjs05 <jjs-0005@outlook.jp>
2014-05-12 16:12:09 +0900

    Exer commit2

---
+++ src/jp/co/jjs/java_seminar/exercise_20140512_2/Exercise2.java
@@ -0,0 +1,14 @@
+package jp.co.jjs.java_seminar.exercise_20140512_2;
+
+public class Exercise2 {
+
+    public static void main(String[] args) {
+
+        String str = "今日の天気は曇りです。";
+        String str2 = "今日の気温は";
+        int kion = 24;
+
+        System.out.println(str + str2 + kion +"℃です");
+    }
+
+}
