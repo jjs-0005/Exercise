53c1475553a4ba6331ef36c2bc65584357d484e6
jjs05 <jjs-0005@outlook.jp>
2014-05-12 15:50:33 +0900

    Exer commit

---
+++ src/jp/co/jjs/java_seminar/exercise_20140512_1/Exercise1.java
@@ -0,0 +1,28 @@
+package jp.co.jjs.java_seminar.exercise_20140512_1;
+
+public class Exercise1 {
+
+    public static void main(String[] args) {
+
+
+        byte   b = 1;
+        short  s = 1000;
+        int    i = 10000;
+        long   l = 100000;
+        char   c = '1';
+        double d = 12.1;
+        float  f = 45.0f;
+        boolean boo = true;
+
+        System.out.println("byte =" + b);
+        System.out.println("short =" + s);
+        System.out.println("int =" + i);
+        System.out.println("long =" + l);
+        System.out.println("char =" + c);
+        System.out.println("double =" + d);
+        System.out.println("float =" + f);
+        System.out.println("boolean =" + boo);
+
+    }
+
+}
