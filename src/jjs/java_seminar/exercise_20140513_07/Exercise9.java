3de66f1f98cebced533866d7a00770d51f0cbc8a
jjs05 <jjs-0005@outlook.jp>
2014-05-13 14:44:29 +0900

    Exer9_commit

---
+++ src/jjs/java_seminar/exercise_20140513_07/Exercise9.java
@@ -0,0 +1,25 @@
+package jjs.java_seminar.exercise_20140513_07;
+
+public class Exercise9 {
+
+    public static void main(String[] args) {
+
+        int[] a = new int[5];
+
+        int[] b = {1, 2, 3, 4, 5};
+
+        String[] c = new String[5];
+
+        String[] d = {"a", "b", "c", "d", "e"};
+
+        int n = 0;
+
+        n =a.length;
+
+        System.out.println("配列aの要素数 " + n);
+
+        System.out.println(d[0]);
+
+    }
+
+}
