3073aa5de480683ccf995a9ca03a87b3032013db
jjs05 <jjs-0005@outlook.jp>
2014-05-13 14:25:47 +0900

    Self_commit

---
+++ src/jp/co/jjs/java_seminar/self_study/q1_12/Self_Study_1_12.java
@@ -0,0 +1,22 @@
+package jp.co.jjs.java_seminar.self_study.q1_12;
+
+public class Self_Study_1_12 {
+
+    public static void main(String[] args) {
+
+        char ch;
+        short s;
+        long l;
+        float f;
+
+        /*
+         *
+         *  f / ch - (s * 1)の式全体の型はfloat
+         *
+         *
+         *
+         */
+
+    }
+
+}
