be201ea0120d36a1db6c61b4737d7e865a4485ea
jjs05 <jjs-0005@outlook.jp>
2014-05-13 10:48:42 +0900

    Exer4_commit

---
+++ src/jjs/java_seminar/exercise_20140513_02/Exercise4.java
@@ -0,0 +1,15 @@
+package jjs.java_seminar.exercise_20140513_02;
+
+public class Exercise4 {
+
+    public static void main(String[] args) {
+
+        short a = 1;
+        short b = 2;
+        int sum = 0;
+
+        sum = a + b;
+        System.out.println(sum);
+    }
+
+}
