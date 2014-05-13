c232f0c66fdd150c6695b36de5386cfa0567f6a6
jjs05 <jjs-0005@outlook.jp>
2014-05-13 10:34:47 +0900

    Exer3_commit

---
+++ src/jjs/java_seminar/exercise_20140513_01/Exercise3.java
@@ -0,0 +1,31 @@
+package jjs.java_seminar.exercise_20140513_01;
+
+public class Exercise3 {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 3;
+        int sum = 0;
+
+
+
+        sum = a + b;
+        System.out.println("10 + 3 = " + sum );
+
+        sum = a - b;
+        System.out.println("10 - 3 = " + sum );
+
+        System.out.print("10 / 3 = ");
+        System.out.println((float)a / b  );
+
+        sum = a / b;
+        System.out.println("10 / 3 = " + sum );
+
+        sum = a % b;
+        System.out.println("10 % 3 = " + sum );
+
+
+    }
+
+}
