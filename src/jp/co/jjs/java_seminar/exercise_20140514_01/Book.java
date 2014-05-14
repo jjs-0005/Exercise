5a8a4880fea82af8e7b2e1a2d084169f4eb0374e
jjs05 <jjs-0005@outlook.jp>
2014-05-14 11:17:35 +0900

    book_commit

---
+++ src/jp/co/jjs/java_seminar/exercise_20140514_01/Book.java
@@ -0,0 +1,12 @@
+package jp.co.jjs.java_seminar.exercise_20140514_01;
+
+public class Book {
+
+    int     bookid;     //管理ID
+    String  bookname;   //名前
+    String  authord;    //著者
+    String  genre;      //ジャンル
+    boolean bookrental; //貸出状況
+
+
+}
